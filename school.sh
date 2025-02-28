901f477fb79c06d49e5d6c75b4af279e026e3372
Terence Wang <twang682@wisc.edu>
2025-02-27 22:15:21 -0600

    Updated school.sh to compute average TotalAssessedValue for MADISON SCHOOLS

--- school.sh
+++ school.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | \
+    grep "MADISON SCHOOLS" | \
+    cut -d ',' -f7 | \
+    { sum=0; count=0; while read n; do sum=$((sum + n)); count=$((count + 1)); done; echo "Total Assessed Value: $sum"; echo "Average Total Assessed Value: $(echo "scale=2; $sum / $count" | bc)"; }
+
+
