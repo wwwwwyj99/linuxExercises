56ac207ab5a0466ac15134a60632e5f0b20d0ce2
Elaine Wan <ywan48@wisc.edu>
2025-02-28 20:17:57 -0600

    Elaine modified digits.sh again

--- digits.sh
+++ digits.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sum = 0
+sum=0
 
 for num in {1000..2000}; do
     if [[ $num =~ ^[01]+$ ]]; then
-	sum = $((sum + sum))
+	sum=$((sum + sum))
     fi
 done
 
-echo "The sum is $sum"
+echo "The sum isshe $sum"
